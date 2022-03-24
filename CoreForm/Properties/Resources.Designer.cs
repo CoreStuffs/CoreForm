4414d8a38291e122d65acbd3a6aaa920b82cfc2e
Jean-François Gueldre <jean-francois.gueldre@globaljetlux.com>
2022-03-24 19:11:11 +0100

    Back to VueCliMiddleWare 3.0.0

--- CoreForm/Properties/Resources.Designer.cs
+++ CoreForm/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CoreForm.Properties {
+namespace CoreStuffs.CoreForm.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CoreForm.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CoreForm.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CoreStuffs.CoreForm.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
