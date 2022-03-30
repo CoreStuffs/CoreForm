9fb9fbf0324b7641157f8bd8970553eb1dde7b85
Jeff Gueldre <jeffaries@users.noreply.github.com>
2022-03-31 00:56:57 +0200

    inputType

--- CoreForm/ClientApp/src/fields/textfield/control.vue
+++ CoreForm/ClientApp/src/fields/textfield/control.vue
@@ -3,15 +3,14 @@
         <label :for="schema.id" class="uk-form-label">{{ schema.label }}<div class="required-tag" v-if="$isrequired"></div></label>
         <div class="uk-form-controls">
             <input
-                   type="email"
+                   :type="inputType"
                    :placeholder="schema.placeholder"
                    class="uk-input uk-form-small"
                    v-bind:class="{ 'uk-form-danger': $error }"
                    :id="schema.id"
                    v-model="data"
-                   @input="updateInput" />
+                   @input="updateInput" />{{inputType}}
         </div>
-        <hr/>
         <span v-if="$errorMessage" class="error-message">{{ $errorMessage }}</span>
     </div>
 </template>
@@ -22,7 +21,12 @@
         data() {
             return { data: this.value };
         },
-        methods: {      
+        computed: {
+            inputType: function () {
+                return "text";
+            },
+        },
+        methods: {
             updateInput: function () {
                 this.$emit("input", this.$el.getElementsByTagName("input")[0].value);
                 if (this.$validation) this.$validation.$touch();
